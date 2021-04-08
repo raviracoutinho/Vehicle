280eafc38a684d4c5eee4b7c3f0a766f8c3b8504
sysadmin <sysadmin@bcalab1-m19>
2021-04-08 09:07:18 +0530

    I'm sharing my code with you

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+
+public class Car {
+	
+	String brand;
+	int speed;
+	Car (String brand, int gears)
+	{
+	this.brand = brand;
+	this.speed = speed;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

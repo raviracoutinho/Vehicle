779aaece7f26eca7bb7a7b3d873b04ba8e3cbbc6
sysadmin <sysadmin@bcalab1-m19>
2021-04-08 09:24:20 +0530

    Added contructor

--- src/Car.java
+++ src/Car.java
@@ -8,7 +8,10 @@ public class Car {
 	this.brand = brand;
 	this.speed = speed;
 	}
-
+	void change_speed(int speed)
+	{
+	this.speed = speed;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
